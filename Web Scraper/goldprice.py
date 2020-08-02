6bc70503baad881cb245c7ad93d171964225ba34
ayushab06 <ab06ayush@gmail.com>
2020-08-02 10:26:41 +0530

    added a txt file for storing o/ps and respective changes in .py file

--- Web Scraper/goldprice.py
+++ Web Scraper/goldprice.py
@@ -10,10 +10,10 @@ try:
     data = requests.get("https://www.fresherslive.com/gold-rate-today/"+city.lower()) 
     soup = BS(data.text, 'html.parser') 
 
-    price = soup.find("td",class_="center-text").text
+    price = soup.find("td",class_="center-text").text[1:]
     cost = soup.findAll("td",class_="center-text")[1].text
 
-    print("1gm 22k gold price in " + city.upper().capitalize()+ ": "+price+" change: "+cost)
+    message=("1gm 22k gold price in " + city.upper().capitalize()+ ": Rs."+price+" change: "+cost)
     
     #for testing if it works for negative change change value of cost
     
@@ -21,7 +21,13 @@ try:
     	winsound.Beep(2000, 1250)		#creates sound of frequency 2000hz for 1.25 seconds
 
 except:
-    print("Not Found!")
+    message=("Not Found!")
+
+file_i=open("data.txt","a")
+file_i.write(message)
+file_i.close()
+print(message)
+
 
 print()
 input("Press enter to exit.")
