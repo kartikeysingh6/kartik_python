4025138139580a41818cc226280fba7bc4e4ceb4
Kartikey Singh <59006007+kartikeysingh6@users.noreply.github.com>
2021-05-28 11:36:04 +0530

    CoWin API Alert App (Pincode)
    A simple python application that extracts information from public API provided by government of India at API Setu portal, all you have to to is to replace the pincode with your required pincode and you can also change loop timer, it will generate Name, Address, Total Capacity, Capacity of Dose 1 and Slots of those Hospitals where Total capacity>0 or Total dose 1 capacity >0. This application will run in a loop every 30 seconds(default) and will give Alert(beeping sound) when output is found.

---
+++ CoWinAlertPincode.py
@@ -0,0 +1,33 @@
+import requests
+import json
+import winsound
+import time
+from datetime import date
+from datetime import datetime
+
+#change this according to your need
+pin="110006"
+loop_time_inseconds=30
+
+while(1):
+	currtime = datetime.now().strftime("%H:%M:%S")
+	today = date.today().strftime("%d-%m-%y")
+
+	apiURL="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode="+pin+"&date="+today
+	response = requests.get(apiURL, headers={"accept-language": "en-US"})
+
+	print("Current Time:", currtime+"\n")
+	print(today+"\n")
+
+	for place in response.json()['sessions']:
+		if(place['available_capacity']>0 or place['available_capacity_dose1']>0):
+			winsound.Beep(1000, 500)
+			winsound.Beep(1000, 500)
+			print(place['name'],", "+place['address'])
+			print("Total Capacity: ",place['available_capacity'], ", Dose1: ",place['available_capacity_dose1'])
+			print(place['slots'])
+			print()
+		else:
+			print("No available Dose")
+
+	time.sleep(loop_time_inseconds)
