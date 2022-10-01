35ad4baf97adf515b1af016294df29948e6ae88b
Ayush Singh <65663313+ayushab06@users.noreply.github.com>
2022-10-01 11:55:11 +0530

    abstraction in java

---
+++ abstraction.java
@@ -0,0 +1,25 @@
+// Abstract class
+abstract class Animal {
+  // Abstract method (does not have a body)
+  public abstract void animalSound();
+  // Regular method
+  public void sleep() {
+    System.out.println("Zzz");
+  }
+}
+
+// Subclass (inherit from Animal)
+class Pig extends Animal {
+  public void animalSound() {
+    // The body of animalSound() is provided here
+    System.out.println("The pig says: wee wee");
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    Pig myPig = new Pig(); // Create a Pig object
+    myPig.animalSound();
+    myPig.sleep();
+  }
+}
