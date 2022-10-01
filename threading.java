6f251dd87bc35af86dd3100de5cfb222cf373ddf
Ayush Singh <65663313+ayushab06@users.noreply.github.com>
2022-10-01 12:03:19 +0530

    implements multithreading in java

---
+++ threading.java
@@ -0,0 +1,28 @@
+class MultithreadingDemo extends Thread {
+    public void run()
+    {
+        try {
+            // Displaying the thread that is running
+            System.out.println(
+                "Thread " + Thread.currentThread().getId()
+                + " is running");
+        }
+        catch (Exception e) {
+            // Throwing an exception
+            System.out.println("Exception is caught");
+        }
+    }
+}
+ 
+// Main Class
+public class Multithread {
+    public static void main(String[] args)
+    {
+        int n = 8; // Number of threads
+        for (int i = 0; i < n; i++) {
+            MultithreadingDemo object
+                = new MultithreadingDemo();
+            object.start();
+        }
+    }
+}
